58365c0c8817c3a3dda3d98d4ea1bfa5f9d31e3b
David_Le <Davidnhi@hotmail.com>
2025-08-07 15:56:52 +0200

    update v.1

---
+++ main.py
@@ -0,0 +1,18 @@
+import math
+import random
+
+class main:
+    def __init__(self):
+        self.value = random.randint(1, 100)
+
+class snake:
+    def __init__(self):
+        self.length = 3
+        self.position = (0, 0)
+
+    def hit():
+        game_over = True
+        print("Game Over!")
+    
+    def move(self, direction):
+        
