ea87c326e9e655f5d186862f075aad142472014c
David_Le <Davidnhi@hotmail.com>
2025-08-07 17:13:04 +0200

    v.3

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 import matplotlib
 import random
 
+def start_game():
+    print("Starting game...")
+    # Initialize game logic here
+    # For example, set up the snake and food, start the game loop, etc.
+
 class main:
     def __init__(self):
         self.menu = ["Start Game", "Settings", "Exit"]
         self.gride_size = 20
-        self.value = random.randint(1, 100)
         self.inter = 0
         self.snake = snake()
         self.food = food()
 
+    match self.menu:
+        case "start game":
+            start_game()
+        case "settings":
+            pass
+        case "exit":
+            print("Exiting game...")
+            exit()
+
+
 class snake:
     def __init__(self):
+        color("#749351")
         self.length = 3
         self.position = (0, 0)
         self.body = [(0, 0), (0, 1), (0,2)]
 
     def game_over():
         if snake.head in snake.body[1:] | snake.position < max(main.gride_size) | snake.position > snake.position < min(main.gride_size):
-
+            
             print("Game Over!")
             return True
     
@@ -37,4 +52,5 @@ class snake:
 
 class food():
     def __init__(self):
-        pass
+        random.randint(0, main.gride_size - 1)
+        color("#801312")
